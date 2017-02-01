958be92cbd8d0995ed82e3f252ccd22a4d30dbd2
Megan Kostick <mkostick@us.ibm.com>
2017-01-31 16:45:25 -0800

    Allow for command to be run with multiple varied input

--- Sources/CircuitBreaker.swift
+++ Sources/CircuitBreaker.swift
@@ -9,11 +9,13 @@ public class CircuitBreaker {
         case halfopen
         case closed
     }
-
+    
+    public typealias AnyFunction<A, B> = (A) -> B
+    
     private(set) var state: State
     private(set) var failures: Int
     var breakerStats: Stats
-    var command: () -> Void
+    var command: String
     var callback: (_ error: Bool) -> Void
 
     let timeout: Double
@@ -25,10 +27,11 @@ public class CircuitBreaker {
     let dispatchSemaphoreState = DispatchSemaphore(value: 1)
     let dispatchSemaphoreFailure = DispatchSemaphore(value: 1)
 
-    // TODO: Look at using the built in queue (DispatchQueue.main doesn't work)
+    // TODO: Look at using OperationQueue and Operation instead to allow cancelling of tasks
     let queue = DispatchQueue(label: "Circuit Breaker Queue", attributes: .concurrent)
 
-    public init (timeout: Double = 10, resetTimeout: Int = 60, maxFailures: Int = 5, callback: @escaping (_ error: Bool) -> Void, command: @escaping () -> Void) {
+    // command (Take a function, parameters, error completion)
+    public init (timeout: Double = 10, resetTimeout: Int = 60, maxFailures: Int = 5, callback: @escaping (_ error: Bool) -> Void, command: String) {
         self.timeout = timeout
         self.resetTimeout = resetTimeout
         self.maxFailures = maxFailures
@@ -43,20 +46,21 @@ public class CircuitBreaker {
     }
 
     // Run
-    public func run () {
+    public func run<A, B>(f: AnyFunction<A, B>,args: Any) -> B {
         breakerStats.trackRequest()
 
         if state == State.open || (state == State.halfopen && pendingHalfOpen == true) {
-            return fastFail()
+            fastFail()
+            return () as! B
         } else if state == State.halfopen && pendingHalfOpen == false {
             pendingHalfOpen = true
-            return callFunction()
+            return callFunction(f: f, args: args)
         } else {
-            return callFunction()
+            return callFunction(f: f, args: args)
         }
     }
 
-    private func callFunction () {
+    private func callFunction<A, B>(f: AnyFunction<A, B>, args: Any) -> B{
 
         var completed = false
 
@@ -81,9 +85,10 @@ public class CircuitBreaker {
             return
         }
 
-        command()
+        let result = f(args as! A)
+            
         complete(error: false)
-        return
+        return result
     }
 
     private func setTimeout(closure: @escaping () -> ()) {
--- Tests/CircuitBreakerTests/CircuitBreakerTests.swift
+++ Tests/CircuitBreakerTests/CircuitBreakerTests.swift
@@ -22,7 +22,8 @@ class CircuitBreakerTests: XCTestCase {
         ]
     }
     
-    func sum(a: Int, b: Int) -> Int {
+    func sum(a: Int, b: Int) -> (Int) {
+       
         return a + b
     }
     
@@ -41,12 +42,12 @@ class CircuitBreakerTests: XCTestCase {
         return a
     }
     
-    func test() -> Void {}
+    func test(_: Any) -> Void {}
 
     // Create CircuitBreaker, state should be Closed and no failures
     func testDefaultConstructor() {
         
-        let breaker = CircuitBreaker(callback: callback, command: test)
+        let breaker = CircuitBreaker(callback: callback, command: "test")
         
         // Check that the state is Closed
         XCTAssertEqual(breaker.breakerState, CircuitBreaker.State.closed)
@@ -59,7 +60,7 @@ class CircuitBreakerTests: XCTestCase {
     // Create CircuitBreaker with user options set
     func testConstructor() {
         
-        let breaker = CircuitBreaker(timeout: 5.0, resetTimeout: 5, maxFailures: 3, callback: callback, command: test)
+        let breaker = CircuitBreaker(timeout: 5.0, resetTimeout: 5, maxFailures: 3, callback: callback, command: "test")
         
         // Check that the state is Closed
         XCTAssertEqual(breaker.breakerState, CircuitBreaker.State.closed)
@@ -77,7 +78,7 @@ class CircuitBreakerTests: XCTestCase {
     // Create CircuitBreaker with user options set
     func testPartialConstructor() {
         
-        let breaker = CircuitBreaker(timeout: 5.0, resetTimeout: 5, callback: callback, command: test)
+        let breaker = CircuitBreaker(timeout: 5.0, resetTimeout: 5, callback: callback, command: "test")
         
         // Check that the state is Closed
         XCTAssertEqual(breaker.breakerState, CircuitBreaker.State.closed)
@@ -95,7 +96,7 @@ class CircuitBreakerTests: XCTestCase {
     // Should enter open state
     func testForceOpen() {
         
-        let breaker = CircuitBreaker(callback: callback, command: test)
+        let breaker = CircuitBreaker(callback: callback, command: "test")
         
         // Force open
         breaker.forceOpen()
@@ -110,7 +111,7 @@ class CircuitBreakerTests: XCTestCase {
         
         let resetTimeout = 10
         
-        let breaker = CircuitBreaker(timeout: 10.0, resetTimeout: resetTimeout, maxFailures: 10, callback: callback, command: test)
+        let breaker = CircuitBreaker(timeout: 10.0, resetTimeout: resetTimeout, maxFailures: 10, callback: callback, command: "test")
         
         // Force open
         breaker.forceOpen()
@@ -129,10 +130,10 @@ class CircuitBreakerTests: XCTestCase {
     // Should enter open state
     func testFastFail() {
         
-        let breaker = CircuitBreaker(callback: callback, command: test)
+        let breaker = CircuitBreaker(callback: callback, command: "test")
         
         breaker.forceOpen()
-        breaker.run()
+        breaker.run(f: test, args: [])
         
         // Sometimes the Travis timing throws this off
         sleep(3)
@@ -145,7 +146,7 @@ class CircuitBreakerTests: XCTestCase {
     // Should reset failures to 0
     func testResetFailures() {
         
-        let breaker = CircuitBreaker(callback: callback, command: test)
+        let breaker = CircuitBreaker(callback: callback, command: "test")
         
         // Set failures
         breaker.numFailures = 10
@@ -164,10 +165,10 @@ class CircuitBreakerTests: XCTestCase {
     // Should enter closed state from halfopen state after a success
     func testHalfOpenSuccess() {
     
-        let breaker = CircuitBreaker(callback: callback, command: test)
+        let breaker = CircuitBreaker(callback: callback, command: "test")
     
         breaker.forceHalfOpen()
-        breaker.run()
+        breaker.run(f: test, args: [])
     
         // Check that state is now closed
         XCTAssertEqual(breaker.breakerState, CircuitBreaker.State.closed)
@@ -177,15 +178,12 @@ class CircuitBreakerTests: XCTestCase {
     // Execute method successfully
     func testFunctionCall() {
 
-        var result = 0
-
-        let breaker = CircuitBreaker(callback: callback) {
-            result = self.sum(a: 1, b: 2)
-        }
+        let breaker = CircuitBreaker(callback: callback, command: "sum")
         
-        breaker.run()
+        let result = breaker.run(f: sum, args: (a: 1, b: 2))
         
         XCTAssertEqual(breaker.breakerState, CircuitBreaker.State.closed)
+        
         XCTAssertEqual(result, 3)
         
     }
@@ -193,14 +191,10 @@ class CircuitBreakerTests: XCTestCase {
     // Print Stats snapshot
     func testStatsSnapshot() {
         
-        var result = 0
-        
-        let breaker = CircuitBreaker(callback: callback) {
-            result = self.sum(a: 1, b: 2)
-        }
+        let breaker = CircuitBreaker(callback: callback, command: "sum")
         
         // TODO: Do something more meaningful here
-        breaker.run()
+        let result = breaker.run(f: sum, args: (a: 1, b: 2))
         
         breaker.snapshot()
         
@@ -212,13 +206,9 @@ class CircuitBreakerTests: XCTestCase {
     // Test timeout
     func testTimeout() {
         
-        var result = 0
+        let breaker = CircuitBreaker(timeout: 5.0, callback: callback, command: "time")
         
-        let breaker = CircuitBreaker(timeout: 5.0, callback: callback) {
-            result = self.time(a: 1, seconds: 11)
-        }
-        
-        breaker.run()
+        let result = breaker.run(f: time, args: (a: 1, seconds: 11))
         
         // TODO: Still gets the result, is this okay???
         XCTAssertEqual(result, 1)
@@ -227,15 +217,11 @@ class CircuitBreakerTests: XCTestCase {
     
     // Test timeout and reset
     func testTimeoutReset() {
-        
-        var result = 0
         let resetTimeout = 10
         
-        let breaker = CircuitBreaker(timeout: 5.0, resetTimeout: resetTimeout, maxFailures: 1, callback: callback) {
-            result = self.time(a: 1, seconds: 11)
-        }
+        let breaker = CircuitBreaker(timeout: 5.0, resetTimeout: resetTimeout, maxFailures: 1, callback: callback, command: "time")
         
-        breaker.run()
+        let result = breaker.run(f: time, args: (a: 1, seconds: 11))
         
         sleep(UInt32(resetTimeout + 2))
         
