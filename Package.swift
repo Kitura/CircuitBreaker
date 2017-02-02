a399102ae4793875d9e67066d6daa3ec8621c083
Megan Kostick <mkostick@us.ibm.com>
2017-02-02 14:10:18 -0800

    Update to HeliumLogger 1.6

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "CircuitBreaker",
     dependencies: [
-	.Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 5),
+	.Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 6),
     ]
 )
