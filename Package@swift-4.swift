636acea26bb468a8fbee4887762d6c6abe375dfa
Quan Vo <quan.vo@ibm.com>
2017-09-12 10:15:13 -0500

    Fix dependency versioning in Package@swift-4.swift

--- Package@swift-4.swift
+++ Package@swift-4.swift
@@ -14,7 +14,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/IBM-Swift/LoggerAPI.git", from: "1.7.1"),
+        .package(url: "https://github.com/IBM-Swift/LoggerAPI.git", .upToNextMinor(from: "1.7.0")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target defines a module or a test suite.
