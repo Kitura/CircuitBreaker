3340f0a107709528821c4b907a64c2245cb37ff1
Aaron Liberatore <aml68@duke.edu>
2017-09-27 15:34:40 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -28,7 +28,7 @@ let package = Package(
       )
     ],
     dependencies: [
-        .package(url: "https://github.com/IBM-Swift/LoggerAPI.git", .upToNextMajor(from: "1.0.0"))
+        .package(url: "https://github.com/IBM-Swift/LoggerAPI.git", .upToNextMajor(from: "2.0.0"))
     ],
     targets: [
       .target(
