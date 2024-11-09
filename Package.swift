57869619f69695532726f2bec75233cca51ec5ec
lahariganti <gplahari@gmail.com>
2024-11-09 18:10:29 +0100

    Update packages (#58)

--- Package.swift
+++ Package.swift
@@ -28,7 +28,7 @@ let package = Package(
       )
     ],
     dependencies: [
-        .package(url: "https://github.com/Kitura/LoggerAPI.git", .upToNextMajor(from: "1.9.200"))
+        .package(url: "https://github.com/Kitura/LoggerAPI.git", from: "2.0.0")
     ],
     targets: [
       .target(
